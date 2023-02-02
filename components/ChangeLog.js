2ba452fcd3701d888929cc60c71c21aea6913520
Mikaël H-Deschamps <mikael.hubert-deschamps@usherbrooke.ca>
2023-02-02 12:36:34 -0500

    Added meta description tag and a meaningful title

--- components/ChangeLog.js
+++ components/ChangeLog.js
@@ -1,38 +1,5 @@
 import React from "react"
 import { useFetch } from "react-async"
-import { useAsync } from "react-async"
-
-/*async function getCommitsData() {
-    const res = await fetch("https://api.github.com/repos/mikaelhd/IGE511-TP1-HUBM2601/commits?author=mikael.hubert-deschamps@usherbrooke.ca",
-        {
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            }
-        })/!*.then((response) => response.json())
-        .then((data) => commitList = data);*!/
-
-    if (!res.ok) {
-        throw new Error("oops");
-    }
-
-    var temp = res.json();
-    var commitAuthor = JSON.parse(temp).commit.author
-
-    /!*return res.json();*!/
-    return commitAuthor[0].toString();
-}*/
-
-const loadCommits = async (/*{ user }, { repo }, { author }*/) => {
-    const res = await fetch(`https://api.github.com/repos/mikaelhd/IGE511-TP1-HUBM2601/commits?author=mikael.hubert-deschamps@usherbrooke.ca`);
-    if (!res.ok) throw new Error(res.statusText);
-
-    const temp = res.json()
-    console.log(temp)
-    return temp
-}
-
-
 const Commits = () => {
     const { data, error } = useFetch(`https://api.github.com/repos/mikaelhd/IGE511-TP1-HUBM2601/commits?author=mikael.hubert-deschamps@usherbrooke.ca`, {
         headers: { accept: "application/json" },
@@ -44,11 +11,13 @@ const Commits = () => {
                 committed by: {commitMessage.commit.author.name},
                 date: {new Date(commitMessage.commit.author.date).toLocaleDateString('en-CA')}</code></li>);
 
-        return (<ol className="changelog"><b>Commit list:</b>
-                {listCommits}</ol>)}
+        return (<div>
+            <b>Commit list:</b>
+            <ol className="changelog">
+                {listCommits}</ol>
+        </div>)}
     return null
 }
-
 
 export default function ChangeLog() {
     return <Commits />
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import YoutubeEmbed from "@components/YoutubeEmbed";
 import ChangeLog from "@components/ChangeLog";
+import Image from "next/image";
+import udsImage from "./uds.jpg"
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
           <meta name="google-site-verification" content="FWi9T4X9ZUYXwnTSFiPtlNIi0ktwvYUsYWjYSKKckzY" />
-        <title>hwygowerhsdf</title>
+          <meta name="description" content="hwygowerhsdf is a secret organization from within UDS that uncovers the
+          truth about the school's secrets"/>
+        <title>The truth about hwygowerhsdf - What they are hiding from us</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Header title="hwygowerhsdf - Top 1 ou rien " />
-          <YoutubeEmbed embedId="dQw4w9WgXcQ" />
-        <p className="description">
+        <Header title="What is hwygowerhsdf ? - Read below for more information" />
+          <Image src={udsImage} width={'512'} height={'512'} alt={"UDS' hidden logo"}/>
+          <p className="description">
             hwygowerhsdf - On sait jamais... mieux vaut spammer le string partout!
         </p>
       </main>
